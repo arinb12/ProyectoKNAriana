e6e75319c5c71775541a3c384cd2b74140713e10
arinb12 <anunez00854@ufide.ac.cr>
2025-02-23 17:46:18 -0600

    Cambios al receso semana1y2

---
+++ KN_ProyectoClase/KN_ProyectoClase/Controllers/PrincipalController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//La vista llama al controlador y lo que le envia o lo que recibe es un modelo
+namespace KN_ProyectoClase.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        [HttpGet]   //Abrir y mostrar la vista. Siempre lo llama un hipervinculo
+        public ActionResult Inicio()
+        {  
+            return View();
+        }
+
+        [HttpGet]
+
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+    }
+}
