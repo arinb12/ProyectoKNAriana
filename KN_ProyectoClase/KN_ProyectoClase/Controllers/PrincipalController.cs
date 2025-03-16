1979bd70c2aa3e3e23f30ab978197b0cb24655cc
arinb12 <anunez00854@ufide.ac.cr>
2025-03-16 15:07:04 -0600

    class8

--- KN_ProyectoClase/KN_ProyectoClase/Controllers/PrincipalController.cs
+++ KN_ProyectoClase/KN_ProyectoClase/Controllers/PrincipalController.cs
@@ -1,127 +1,243 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using KN_ProyectoClase.BaseDatos;
+﻿using KN_ProyectoClase.BaseDatos;
 using KN_ProyectoClase.Models;
-//La vista llama al controlador y lo que le envia o lo que recibe es un modelo
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Mvc;
+
 namespace KN_ProyectoClase.Controllers
 {
     public class PrincipalController : Controller
     {
+        RegistroErrores error = new RegistroErrores();
 
         #region RegistrarCuenta
 
-        [HttpGet]//Abrir y mostrar la vista. Siempre lo llama un hipervinculo
-
+        [HttpGet]
         public ActionResult RegistrarCuenta()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Get RegistrarCuenta");
+                return View("Error");
+            }
         }
 
-        [HttpPost]//Despues de un submit button
-
+        [HttpPost]
         public ActionResult RegistrarCuenta(UsuarioModel model)
         {
-            //BD
-            //EF usando LinQ
-            using (var context = new KN_DBEntities())
+            try
             {
-                //Usuario tabla = new Usuario();
-                //tabla.Identificacion = model.Identificacion;
-                //tabla.Contrasenna = model.Contrasenna;
-                //tabla.Nombre = model.Nombre;
-                //tabla.Correo = model.Correo;
-                //.Estado = true;
-                //tabla.IdPerfil = 2;
-
-                //context.Usuario.Add(tabla);
-                //var result = context.SaveChanges();
-
-                //EF usando Procedimientos Almacenados
-                var result = context.RegistrarCuenta(model.Identificacion, model.Contrasenna, model.Nombre, model.Correo);
-
-                if(result > 0)
-                    return RedirectToAction("IniciarSesion", "Principal");
-                else
+                //EF utilizando LinQ
+                using (var context = new KN_DBEntities())
                 {
-                    ViewBag.Mensaje = "Su información no se ha podido registrar correctamente";
-                    return View();
+                    //Usuario tabla = new Usuario();
+                    //tabla.Identificacion = model.Identificacion;
+                    //tabla.Contrasenna = model.Contrasenna;
+                    //tabla.Nombre = model.Nombre;
+                    //tabla.Correo = model.Correo;
+                    //tabla.Estado = true;
+                    //tabla.IdPerfil = 2;
+
+                    //context.Usuario.Add(tabla);
+                    //var result = context.SaveChanges();
+
+                    //EF utilizando Procedimientos Almacenados
+                    var result = context.RegistrarCuenta(model.Identificacion, model.Contrasenna, model.Nombre, model.Correo);
+
+                    if (result > 0)
+                        return RedirectToAction("IniciarSesion", "Principal");
+                    else
+                    {
+                        ViewBag.Mensaje = "Su información no se ha podido registrar correctamente";
+                        return View();
+                    }
                 }
-
             }
-
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Post RegistrarCuenta");
+                return View("Error");
+            }
         }
+
         #endregion
 
         #region IniciarSesion
 
         [HttpGet]
-
         public ActionResult IniciarSesion()
         {
-            return View();
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Get IniciarSesion");
+                return View("Error");
+            }
         }
 
         [HttpPost]
         public ActionResult IniciarSesion(UsuarioModel model)
         {
-            //EF usando LinQ
-            using (var context = new KN_DBEntities())
+            try
             {
-
-                //EF usando LinQ
-                //var info = context.Usuario.
-                //Where(x => x.Identificacion == model.Identificacion
-                //&& x.Contrasenna == model.Contrasenna
-                //&& x.Estado == true).FirstOrDefault();
-
-                //EF usando Procedimientos Almacenados
-                  var info = context.IniciarSesion(model.Identificacion, model.Contrasenna).FirstOrDefault();
-
-                if (info != null)
+                using (var context = new KN_DBEntities())
                 {
-                    Session["NombreUsuario"] = info.NombreUsuario;
-                    Session["NombrePerfilUsuario"] = info.NombrePerfil;
-                    Session["IdPerfilUsuario"] = info.IdPerfil;
-                    return RedirectToAction("Inicio","Principal");
-                }
-                else
-                {
-                    ViewBag.Mensaje = "Su información no se ha podido validar correctamente";
-                    return View();
+                    //EF utilizando LinQ
+                    //var info = context.Usuario.
+                    //Where(x => x.Identificacion == model.Identificacion
+                    //&& x.Contrasenna == model.Contrasenna
+                    //&& x.Estado == true).FirstOrDefault();
+
+                    //EF utilizando Procedimientos Almacenados
+                    var info = context.IniciarSesion(model.Identificacion, model.Contrasenna).FirstOrDefault();
+
+                    if (info != null)
+                    {
+                        Session["IdUsuario"] = info.Id;
+                        Session["NombreUsuario"] = info.NombreUsuario;
+                        Session["NombrePerfilUsuario"] = info.NombrePerfil;
+                        Session["IdPerfilUsuario"] = info.IdPerfil;
+                        return RedirectToAction("Inicio", "Principal");
+                    }
+                    else
+                    {
+                        ViewBag.Mensaje = "Su información no se ha podido validar correctamente";
+                        return View();
+                    }
                 }
             }
-
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Post IniciarSesion");
+                return View("Error");
+            }
         }
 
         #endregion
 
+        #region RecuperarContrasenna
 
-        [HttpGet]   //Abrir y mostrar la vista. Siempre lo llama un hipervinculo
-        public ActionResult Inicio()
-        {  
-            return View();
+        [HttpGet]
+        public ActionResult RecuperarContrasenna()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Get RecuperarContrasenna");
+                return View("Error");
+            }
         }
 
+        [HttpPost]
+        public ActionResult RecuperarContrasenna(UsuarioModel model)
+        {
+            try
+            {
+                using (var context = new KN_DBEntities())
+                {
+                    var info = context.Usuario.Where(x => x.Correo == model.Correo
+                                                       && x.Estado == true).FirstOrDefault();
+
+                    if (info != null)
+                    {
+                        var codigoTemporal = CrearCodigo();
+
+                        info.Contrasenna = codigoTemporal;
+                        context.SaveChanges();
+
+                        var notificacion = EnviarCorreo(info, codigoTemporal, "Acceso al sistema KN");
+
+                        if (notificacion)
+                            return RedirectToAction("IniciarSesion", "Principal");
+                    }
+
+                    ViewBag.Mensaje = "Su acceso no se ha podido reestablecer correctamente";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Post RecuperarContrasenna");
+                return View("Error");
+            }
+        }
+
+        #endregion
+
+        [HttpGet]
+        public ActionResult Inicio()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Get Inicio");
+                return View("Error");
+            }
+        }
 
         [HttpGet]
         public ActionResult CerrarSesion()
         {
-            Session.Clear();
-            return RedirectToAction("Inicio","Principal");
+            try
+            {
+                Session.Clear();
+                return RedirectToAction("Inicio", "Principal");
+            }
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Get CerrarSesion");
+                return View("Error");
+            }
         }
 
-
-        [HttpGet]
-
-        public ActionResult RecuperarContrasenna()
+        private string CrearCodigo()
         {
-            return View();
+            int length = 5;
+            const string valid = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
         }
 
-        //PENDIENTE POST RECUPERAR CONTRASENA
+        private bool EnviarCorreo(Usuario info, string codigo, string titulo)
+        {
+            string cuenta = ConfigurationManager.AppSettings["CorreoNotificaciones"].ToString();
+            string contrasenna = ConfigurationManager.AppSettings["ContrasennaNotificaciones"].ToString();
 
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(cuenta);
+            message.To.Add(new MailAddress(info.Correo));
+            message.Subject = titulo;
+            message.Body = $"Hola {info.Nombre}, por favor utilice el siguiente código para ingresar al sistema: {codigo}";
+            message.Priority = MailPriority.Normal;
+            message.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient("smtp.office365.com", 587);
+            client.Credentials = new System.Net.NetworkCredential(cuenta, contrasenna);
+            client.EnableSsl = true;
+            client.Send(message);
+            return true;
+        }
     }
 }
---
+++ KN_ProyectoClase/KN_ProyectoClase/Models/RegistroErrores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KN_ProyectoClase.BaseDatos;
+
+namespace KN_ProyectoClase.Models
+{
+    public class RegistroErrores
+    {
+        public void RegistrarError(string Mensaje, string Origen)
+        {
+            using (var context = new KN_DBEntities())
+            {
+                var IdUsuario = HttpContext.Current.Session["IdUsuario"].ToString() ?? "0";
+                context.RegistrarError(long.Parse(IdUsuario), Mensaje, Origen);
+            }
+        }
+    }
+}
